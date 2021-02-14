b41a15122f50f162c5a5e2cda27d0914e8c0f614
Jung Jun Won <wjdwnsdnjs13@naver.com>
2021-02-14 07:21:43 +0000

    0214

--- algorithm/DP/make_a_number_1.py
+++ algorithm/DP/make_a_number_1.py
@@ -4,10 +4,10 @@ x = int(input())
 dp = [0] * 30001
 for i in range(2, x+1):
     dp[i] = dp[i-1] + 1
-    if i%2 == 0:
+    if i % 2 == 0:
         dp[i] = min(dp[i//2] + 1, dp[i])
-    if i%3 == 0:
+    if i % 3 == 0:
         dp[i] = min(dp[i//3] + 1, dp[i])
-    if i%5 == 0:
+    if i % 5 == 0:
         dp[i] = min(dp[i//5] + 1, dp[i])
 print(dp[x])
