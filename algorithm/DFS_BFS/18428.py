b83f1e47bf200e9a6b6f8115f5fb70190159bc2d
wjdwnsdnjs13 <wjdwnsdnjs13@naver.com>
2021-04-03 11:31:34 +0000

    0403

--- algorithm/DFS_BFS/18428.py
+++ algorithm/DFS_BFS/18428.py
@@ -1,4 +1,3 @@
-# 아직 오류 발생함. combinations 사용하는 방법 익히기
 # 이코테 351p 백준 18428 감시 피하기
 from itertools import combinations
 
@@ -13,56 +12,55 @@ for i in range(n):
             teacher.append((i,j))
         elif zido[i][j] == 'X':
             blank.append((i,j))
+
 def dfs(x, y, d):
     if d == 0:
-        while y >= 0:
-            if zido[x][y] == 'S':
-                return True
-            if zido[x][y] == 'O':
-                return False
-            y -= 1
-    if d == 1:
         while y < n:
-            if zido[x][y] == 'S':
+            if zido[x][y] == "S":
                 return True
-            if zido[x][y] == 'O':
+            if zido[x][y] == "O":
                 return False
             y += 1
-    if d == 2:
-        while x >= 0:
-            if zido[x][y] == 'S':
-                return True
-            if zido[x][y] == 'O':
-                return False
-            x -= 1
-    if d == 3:
+    elif d == 1:
         while x < n:
-            if zido[x][y] == 'S':
+            if zido[x][y] == "S":
                 return True
-            if zido[x][y] == 'O':
+            if zido[x][y] == "O":
                 return False
             x += 1
+    elif d == 2:
+        while y >= 0:
+            if zido[x][y] == "S":
+                return True
+            if zido[x][y] == "O":
+                return False
+            y -= 1
+    elif d == 3:
+        while x >= 0:
+            if zido[x][y] == "S":
+                return True
+            if zido[x][y] == "O":
+                return False
+            x -= 1
     return False
 
 def process():
-    for x, y in teacher:
+    for x,y in teacher:
         for i in range(4):
             if dfs(x, y, i):
                 return True
     return False
-
+    
 find = False
-for data in combinations(blank, 3):
-    for x,y in data:
-        zido[x][y] = '0'
+for w in combinations(blank, 3):
+    for a, b in w:
+        zido[a][b] = 'O'
     if not process():
         find = True
         break
-    for x, y in data:
-        zido[x][y] = 'X'
-
-
+    for a, b in w:
+        zido[a][b] = 'X'
 if find:
-    print('yes')
+    print("YES")
 else:
-    print('no')
+    print("NO")
