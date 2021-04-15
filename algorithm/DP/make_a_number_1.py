ea9b0a9393dfd70e7753c61dabf5929ec3f85e57
wjdwnsdnjs13 <wjdwnsdnjs13@naver.com>
2021-04-15 06:27:51 +0000

    0415

--- algorithm/DP/make_a_number_1.py
+++ algorithm/DP/make_a_number_1.py
@@ -14,8 +14,8 @@ print(dp[x])
 
 # 백준 1463 1로 만들기
 n = int(input())
-dp = [1] * (n + 1)
-for i in range(4, n + 1):
+dp = [0] * (n + 1)
+for i in range(2, n + 1):
     dp[i] = dp[i-1] + 1
     if i%2 == 0:
         dp[i] = min(dp[i], dp[i//2] + 1)
