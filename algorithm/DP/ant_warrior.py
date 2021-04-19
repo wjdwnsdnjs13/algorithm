0b40d195c2115367d37a8b61c8343306f53411bc
wjdwnsdnjs13 <wjdwnsdnjs13@naver.com>
2021-04-19 01:47:10 +0000

    0419

--- algorithm/DP/ant_warrior.py
+++ algorithm/DP/ant_warrior.py
@@ -10,3 +10,27 @@ for i in range(4, n+1):
     dp[i] = max(dp[i-2], dp[i-3]) + food[i-1]
 
 print(dp)
+
+
+# 재귀를 이용한 풀이
+n = int(input())
+food = list(map(int, input().split()))
+memo = {
+    0 : food[0],
+    1 : food[1],
+    2 : max(food[0]+food[2], food[1])
+}
+
+def thief(x):
+    if x == 1:
+        return memo[0]
+    elif x == 2:
+        return memo[1]
+    if x in memo:
+        return memo[x-1]
+    output =  max(thief(x-1), food[x-1] + thief(x-2))
+    memo[x-1] = output
+    return output
+
+print(thief(n))
+print(memo)
