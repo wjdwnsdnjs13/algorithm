cf10a030ec012852e0dcd0cde0579b87e1490590
JunWonLaptop <wjdwnsdnjs13@naver.com>
2023-03-28 22:29:20 +0900

    Create [1차] 다트 게임.py

---
+++ algorithm/programmers/level1/[1차] 다트 게임.py
@@ -0,0 +1,28 @@
+def solution(dartResult):
+    answer = 0
+    prev = 0
+    i = -1
+    while(i < len(dartResult) - 1):
+        i += 1
+        # for문에서는 다음 스텝이 시작될 때 i가 초기화되어서 +1 되고 시작됨.
+        squre = 1
+        option = 1
+        if dartResult[i + 1] == '0':
+            score = int(dartResult[i:i + 2])
+            i += 2
+        else:
+            score = int(dartResult[i])
+            i += 1
+        if dartResult[i] == "S": squre = 1
+        elif dartResult[i] == "D": squre = 2
+        elif dartResult[i] == "T": squre = 3
+        if i < len(dartResult) - 1:
+            if dartResult[i + 1] == "*":
+                prev *= 2
+                option = 2
+            elif dartResult[i + 1] == "#": option = -1
+        if option != 1: i += 1
+        answer += prev
+        prev = score ** squre * option
+    answer += prev
+    return answer
