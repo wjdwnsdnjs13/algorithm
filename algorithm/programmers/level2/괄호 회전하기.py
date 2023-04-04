ea7f34845b0fbe5dc0ee826ec37314b659744132
JunWonLaptop <wjdwnsdnjs13@naver.com>
2023-04-04 12:40:14 +0900

    Update 괄호 회전하기.py

--- algorithm/programmers/level2/괄호 회전하기.py
+++ algorithm/programmers/level2/괄호 회전하기.py
@@ -1,14 +1,29 @@
 # 14번 케이스 해결 해야됨
-# ({)} 등
+# ({)} 등 -> 해결 완료
 def solution(s):
     answer = 0
     for i in range(len(s) - 1):
         text = s[i:] + s[:i]
         one, two, three = 0, 0, 0
-        for t in text:
-            one += 1 if t == "[" else -1 if t == "]" else 0
-            two += 1 if t == "(" else -1 if t == ")" else 0
-            three += 1 if t == "{" else -1 if t == "}" else 0
+        after = ""
+        for j in range(len(text)):
+            t = text[j]
+            after = text[j + 1] if j < (len(text) - 1) else ""
+            if t == "[":
+                one += 1
+                if after == ")" or after == "}": break
+            elif t == "]":
+                one -= 1
+            elif t == "(":
+                two += 1
+                if after == "]" or after == "}": break
+            elif t == ")":
+                two -= 1
+            elif t == "{":
+                three += 1
+                if after == ")" or after == "]": break
+            elif t == "}":
+                three -= 1
             if one < 0 or two < 0 or three < 0:
                 break
         else:
