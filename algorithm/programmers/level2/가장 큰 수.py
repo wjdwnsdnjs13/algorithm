1dc16b028a13069e43351f17ec52e0121d8c4631
JunWonLaptop <wjdwnsdnjs13@naver.com>
2023-05-21 14:48:09 +0900

    Update 가장 큰 수.py

--- algorithm/programmers/level2/가장 큰 수.py
+++ algorithm/programmers/level2/가장 큰 수.py
@@ -1,11 +1,7 @@
 import itertools
 
 def solution(numbers):
-    # 1. 첫번째자리 순 정렬
-    # 2. 큰 수 순으로 정렬
-    answer = ''
-    numbers.sort(reverse = True, key=lambda x: (str(x)[0], x))
-    for n in numbers:
-        answer += str(n)
-    
-    return answer
+    numbers = list(map(str, numbers))
+    numbers.sort(key = lambda x: (x * 4)[:4], reverse = True)
+    answer = "".join(numbers)
+    return answer if answer[0] != "0" else "0"
