c0ee915e2e0b67215e285c660ed55dcfa344cfbb
JunWonLaptop <wjdwnsdnjs13@naver.com>
2023-05-28 22:11:24 +0900

    Create 기능 개발.py

---
+++ algorithm/programmers/level2/기능 개발.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def solution(progresses, speeds):
+    answer = []
+    progresses = deque(progresses)
+    speeds = deque(speeds)
+    while(progresses):
+        count = 0
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
+        while(progresses and progresses[0] >= 100):
+            progresses.popleft()
+            speeds.popleft()
+            count += 1
+        if count > 0:
+            answer.append(count)
+    return answer
