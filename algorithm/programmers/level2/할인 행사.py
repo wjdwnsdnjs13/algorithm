31ccd58b5df0ce7155315d3231a2c4eaa45f7756
JunWonLaptop <wjdwnsdnjs13@naver.com>
2023-05-29 19:37:38 +0900

    Update 할인 행사.py

--- algorithm/programmers/level2/할인 행사.py
+++ algorithm/programmers/level2/할인 행사.py
@@ -1,15 +1,11 @@
 def solution(want, number, discount):
     answer = 0
-    check = dict()
-    for i in range(len(want)):
-            check[want[i]] = number[i]
-    for i in range(len(discount) - 10):        
-        for j in range(i, i + 10):
-            if discount[j] in check and check[discount[j]] != 0:
-                check[discount[j]] -= 1
-        flag = True
-        for c in check:
-            if check[c] != 0:
-                flag = False
-        answer += (1 if flag else 0)
+    check = []
+    for w, n in zip(want, number):
+        check += [w] * n
+    check.sort()
+    for i in range(len(discount) - 9):
+        tenDayDiscount = sorted(discount[i:i + 10])
+        if check == tenDayDiscount:
+            answer += 1
     return answer
