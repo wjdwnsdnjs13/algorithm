2ca161837adda983b7fa4db8a2235f83e2138279
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2023-11-23 12:02:37 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -1,7 +1,3 @@
 def solution(x):
 #     x 자리 수들의 합으로 x가 나누어져야함.
-    y = 0
-    for i in str(x):
-        y += int(i)
-    if(x%y == 0): return True
-    return False
+    return True if(x % sum(int(i) for i in str(x)) == 0) else False
