37171353d728b1bff8d863194227b28fa716c979
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2023-11-25 15:21:49 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,12 +1,3 @@
 def solution(num):
     answer = 0
-#     짝수면 2로 나누고
-# 홀 수면 3을 곱하고 1을 더함
-# 1이될때까지 반복
-# 500번 반복해도 1이 안되면 -1 반환
-    while(answer <= 500):
-        if(num == 1): return answer
-        answer += 1
-        if(num % 2 == 0): num //= 2
-        else: num = (num * 3) + 1
-    return -1
+    return answer
