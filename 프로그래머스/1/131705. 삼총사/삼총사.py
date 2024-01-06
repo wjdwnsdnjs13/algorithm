c91938b1e765246d8e2c7b162d3c493e1af8b520
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-01-06 20:30:23 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+
+def solution(number):
+    # 삼총사 : 3명 정수 번호의 합 0
+    # return : 삼총사 만드는 경우의 수
+    
+    # 3중 for문 쓰면 바로 해결 가능
+    
+    box = list(combinations(number, 3))
+    answer = 0
+    for b in box:
+        if(not sum(b)): answer += 1
+    
+    return answer
