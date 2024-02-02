ddc1b591af26cf8b93c6d65e0c1055fed3745882
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-02-02 16:40:00 +0900

    [level 2] Title: 타겟 넘버, Time: 10.91 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -9,6 +9,22 @@ def solution(numbers, target):
     # 각 숫자에 (-1)을 곱하거나 (+1)을 곱한 값을 완전 탐색해서 합이 target인 경우 answer += 1
     symbol = list(product([-1, 1], repeat = len(numbers)))
     answer = 0
+    for s in symbol:
+        result = 0
+        for x, y in zip(s, numbers):
+            result += (x * y)
+        answer += 1 if(result == target) else 0
+    return answer
+
+def solution2(numbers, target):
+    # numbers : 사용해야하는 숫자 목록
+    # target : 목표 값
+    # return : target을 만들 수 있는 방법
+    # numbers에 있는 숫자를 더하거나 빼서 타켓 넘버를 만들어야 함.
+    
+    # 각 숫자에 (-1)을 곱하거나 (+1)을 곱한 값을 완전 탐색해서 합이 target인 경우 answer += 1
+    symbol = list(product([-1, 1], repeat = len(numbers)))
+    answer = 0
     
     for s in symbol:
         result = 0
