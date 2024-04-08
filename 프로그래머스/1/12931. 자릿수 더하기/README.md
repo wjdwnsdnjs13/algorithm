c4ab8df50daf1183a4dba321cbf7748c071f919a
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-04-08 22:39:49 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/README.md
+++ 프로그래머스/1/12931. 자릿수 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 자릿수 더하기 - 12931 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12931) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12931?language=java) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.03 ms
+메모리: 74.9 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 21일 23:43:05
+2024년 04월 08일 22:39:47
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        // 어차피 자릿수 계산하거나 stream 쓰려면 String이나 char로 변환해야할 거 같음.
+        int answer = 0;
+        while(n > 0){
+            answer += n%10;
+            n /= 10;
+        }
+        return answer;
+    }
+    
+    public int solution2(int n) {
+        // 어차피 자릿수 계산하거나 stream 쓰려면 String이나 char로 변환해야할 거 같음.
+        String[] number = Integer.toString(n).split("");
+        // System.out.println(Arrays.toString(number));
+        int answer = Arrays.stream(number).mapToInt(s -> Integer.parseInt(s)).sum();
+        return answer;
+    }
+    
+    public int solution3(int n) {
+        // 어차피 자릿수 계산하거나 stream 쓰려면 String이나 char로 변환해야할 거 같음.
+        String[] number = Integer.toString(n).split("");
+        int answer = 0;
+        for(String s: number){
+            answer += Integer.parseInt(s);
+        }
+        return answer;
+    }
+}
