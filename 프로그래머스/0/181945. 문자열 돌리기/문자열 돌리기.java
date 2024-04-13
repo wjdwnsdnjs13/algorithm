7373baf2abab0d9df9f2497385513ad3fe712672
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-04-13 12:34:57 +0900

    [level 0] Title: 문자열 돌리기, Time: 128.53 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] a = sc.next().split("");
+        for(String n: a) System.out.println(n);
+    }
+}
