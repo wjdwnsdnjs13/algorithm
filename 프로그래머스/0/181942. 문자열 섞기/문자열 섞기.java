5405ab0446165c3211edffbc4322f9475a99e043
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-04-14 14:21:31 +0900

    [level 0] Title: 문자열 섞기, Time: 9.55 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,11 +1,19 @@
 class Solution {
-    public String solution(String str1, String str2) {
+    public String solution1(String str1, String str2) {
         String[] strArray1 = str1.split("");
         String[] strArray2 = str2.split("");
         String answer = "";
         for(int i = 0; i < strArray1.length; i++){
             answer += strArray1[i];
             answer += strArray2[i];
+        }
+        return answer;
+    }
+    public String solution(String str1, String str2) {
+        String answer = "";
+        for(int i = 0; i < str1.length(); i++){
+            answer += str1.charAt(i);
+            answer += str2.charAt(i);
         }
         return answer;
     }
