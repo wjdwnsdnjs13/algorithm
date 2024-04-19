838d8f4c38355323b2a33da9069ccf5d57a3feb5
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-04-19 18:17:40 +0900

    [level 0] Title: 수 조작하기 2, Time: 675.39 ms, Memory: 392 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,10 +1,33 @@
+import java.util.*;
+
 class Solution {
-    public String solution(int[] numLog) {
+    Map<Integer, String> map;
+    List<String> list;
+    
+    public String solution1(int[] numLog) {
         int prev = numLog[0];
         String answer = "";
         for(int i = 1; i < numLog.length;i++){
             int dif = numLog[i] - prev;
             answer += (dif == 1)?"w":(dif == -1)?"s":(dif == 10)?"d":"a";
+            prev = numLog[i];
+        }
+        
+        return answer;
+    }
+    
+    public String solution(int[] numLog) {
+        map = new HashMap<>();
+        map.put(1, "w");
+        map.put(-1, "s");
+        map.put(10, "d");
+        map.put(-10, "a");
+        
+        int prev = numLog[0];
+        String answer = "";
+        for(int i = 1; i < numLog.length;i++){
+            int dif = numLog[i] - prev;
+            answer += map.get(dif);
             prev = numLog[i];
         }
         
