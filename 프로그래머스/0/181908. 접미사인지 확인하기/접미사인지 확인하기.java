74cf3bd5a0c2acbd24c1f04984c68fceaca8c0cc
junwon <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-10-08 11:15:04 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.07 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        return my_string.endsWith(is_suffix)?1:0;
+    }
+}
+
+class Solution2 {
+    //13번 케이스 실패함
+    public int solution(String my_string, String is_suffix) {
+        List<String> words = new ArrayList();
+        for(int i = 1; i < my_string.length(); i++){
+            words.add(my_string.substring(my_string.length() - i));
+        }
+        return words.contains(is_suffix)?1:0;
+    }
+}
